f3f6f42586ca2a64171c897c4296759b7259e4d2
Makariy Martyanov <makariy06@mail.ru>
2025-04-24 20:26:41 +0400

    feat: add dfs

--- dfs/a.cpp
+++ dfs/a.cpp
@@ -15,4 +15,20 @@ int main() {
 		g[v].push_back(u);
 		g[u].push_back(v);
 	}
+	queue<int> q;
+	vector<char> used(n);
+	vector<int> d(n, 1e9);
+	d[s] = 0;
+	used[s] = true;
+	q.push(s);
+	while (!q.empty()) {
+		int v = q.front();
+		q.pop();
+		for (int u : g[v]) {
+			if (used[u]) continue;
+			d[u] = d[v] + 1;
+			used[u] = true;
+			q.push(u);
+		}
+	}
 }

c3080ec479f6463c2779f90771aaba3ea615c74e
Makariy Martyanov <makariy06@mail.ru>
2025-05-02 13:59:26 +0400

    feat: add bfs

--- bfs/c.cpp
+++ bfs/c.cpp
@@ -15,4 +15,19 @@ int main() {
 	q.push({ x1, y1 });
 	vector<int> dx = { -2, -1, 1, 2, 2, 1, -1, -2 };
 	vector<int> dy = { 1, 2, 2, 1, -1, -2, -2, -1 };
+        while (!q.empty()) {
+		pair<int, int> v = q.front();
+		int x = v.first;
+		int y = v.second;
+		q.pop();
+		for (int i = 0; i < 8; ++i) {
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+			if ((!(0 <= nx && nx < n && 0 <= ny && ny < n)) || used[nx][ny]) continue;
+			d[nx][ny] = d[x][y] + 1;
+			used[nx][ny] = true;
+			q.push({ nx, ny });
+			p[nx][ny] = { x, y };
+		}
+	}
 }

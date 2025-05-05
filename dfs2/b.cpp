72b9036794468c9ffd75d07f46a5c09eea24699c
Makariy Martyanov <makariy06@mail.ru>
2025-05-05 19:22:05 +0400

    feat: add dfs

--- dfs2/b.cpp
+++ dfs2/b.cpp
@@ -5,6 +5,14 @@
 using namespace std;
 vector<vector<int>> g;
 vector<int> d, p;
+void dfs(int v) {
+	for (int u : g[v]) {
+		if (p[v] == u) continue;
+		p[u] = v;
+		d[u] = d[v] + 1;
+		dfs(u);
+	}
+}
 int main() {
 	int n;
 	cin >> n;

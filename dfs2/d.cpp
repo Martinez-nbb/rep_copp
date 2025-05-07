b18229695753326128deca3b82350fdfd5441467
Makariy Martyanov <makariy06@mail.ru>
2025-05-07 15:16:46 +0400

    feat: add dfs

--- dfs2/d.cpp
+++ dfs2/d.cpp
@@ -10,3 +10,12 @@ bool in(int x, int y) {
 	return 0 <= x && x < n && 0 <= y && y < n;
 }
 vector<vector<char>> used;
+void dfs(int x, int y, char cl) {
+	used[x][y] = cl;
+	for (int i = 0; i < 8; ++i) {
+		int nx = x + dx[i];
+		int ny = y + dy[i];
+		if (!in(nx, ny) || used[nx][ny]) continue;
+		dfs(nx, ny, 3 - cl);
+	}
+}

72b6818f1572daa5c2457e0fbcab01e271be5743
Makariy Martyanov <makariy06@mail.ru>
2025-05-08 15:12:05 +0400

    initial commit

---
+++ dfs2/e.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <vector>
+#include <numeric>
+#include <algorithm>
+using namespace std;
+int n;
+int dx[] = { -2, -1, 1, 2, -2, -1, 1, 2 };
+int dy[] = { 1, 2, 2, 1, -1, -2, -2, -1 };

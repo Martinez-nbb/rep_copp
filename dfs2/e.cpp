82a39fd32c1fe471faa23feeeea317d3ed6ad521
Makariy Martyanov <makariy06@mail.ru>
2025-05-08 15:14:48 +0400

    feat: check for entry into the borders

--- dfs2/e.cpp
+++ dfs2/e.cpp
@@ -6,3 +6,6 @@ using namespace std;
 int n;
 int dx[] = { -2, -1, 1, 2, -2, -1, 1, 2 };
 int dy[] = { 1, 2, 2, 1, -1, -2, -2, -1 };
+bool in(int x, int y) {
+	return 0 <= x && x < n && 0 <= y && y < n;
+}

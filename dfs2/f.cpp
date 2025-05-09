f5f3e48bf48e5c83228df7a50c7dbd50cc17f4db
Makariy Martyanov <makariy06@mail.ru>
2025-05-09 19:24:55 +0400

    initial commit

---
+++ dfs2/f.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <numeric>
+using namespace std;
+vector<vector<char>> g(8, vector<char>(8, 0));
+vector<vector<char>> used(8, vector<char>(8, 0));

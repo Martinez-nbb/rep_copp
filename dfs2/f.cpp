3b80f532d4659ead4c1dfe16b9813bb28b68f891
Makariy Martyanov <makariy06@mail.ru>
2025-05-09 19:27:58 +0400

    feat: check for entry into the borders

--- dfs2/f.cpp
+++ dfs2/f.cpp
@@ -8,3 +8,6 @@ vector<vector<char>> g(8, vector<char>(8, 0));
 vector<vector<char>> used(8, vector<char>(8, 0));
 int dx[] = { 1, 0, -1, 0 };
 int dy[] = { 0, -1, 0, 1 };
+bool in(int x, int y) {
+	return 0 <= x && x < 8 && 0 <= y && y < 8;
+}

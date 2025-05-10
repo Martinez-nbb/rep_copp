15f03e34fa643c75c23a9db97fd7eeebd6f51064
Makariy Martyanov <makariy06@mail.ru>
2025-05-10 13:07:08 +0400

    feat: add binary search

--- binary search/a.cpp
+++ binary search/a.cpp
@@ -16,4 +16,13 @@ int main() {
     	cout << 0;
     	return 0;
     }
+    int y;
+    int m;
+    int l = 1, r = *max_element(a.begin(), a.end());		
+    while (l <= r) {
+    	m = (l + r) / 2;
+    	c = 0;
+    	for (int i = 0; i < n; ++i) {
+    		c += a[i] / m;
+    	}
 }

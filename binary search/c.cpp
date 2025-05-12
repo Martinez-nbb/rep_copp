48a116e2f250c760b58fa3ef85e2a5487a9921bf
Makariy Martyanov <makariy06@mail.ru>
2025-05-12 13:00:45 +0400

    feat: add binary search

--- binary search/c.cpp
+++ binary search/c.cpp
@@ -11,4 +11,23 @@ int main() {
         int n, k;
         cin >> n >> k;
         a.assign(n, 0);
+        for (int j = 0; j < n; ++j) cin >> a[j];
+     	int l = 1;
+     	int  r = 2e9 + 50;
+     	int m, y;
+     	long long c = 0;
+     	while (l <= r) {
+     		c = 0;
+     		m = l + (r - l) / 2;
+     		for (int j = 0; j  < n; ++j) {
+     			if (a[j] < m) {
+     				c += m - a[j];
+     			}
+     		}		
+     		if (k >= c) {
+     			y = m;
+     			l = m + 1;
+     		}	
+     		else r = m - 1;
+     	}
 }

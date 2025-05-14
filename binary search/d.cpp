38555506556e4e34737fc604c314b7a350ada425
Makariy Martyanov <makariy06@mail.ru>
2025-05-14 12:04:31 +0400

    feat: find the maximum and minimum

--- binary search/d.cpp
+++ binary search/d.cpp
@@ -9,4 +9,10 @@ int main(){
 		int n, c, m = 0;
 		cin >> n;
 		a.assign(n, 0);
+        for (int j = 0; j < n; ++j) {
+			cin >> a[j];	
+		}
+		for (int j = 1; j < n; ++j) {
+			c = min(a[j], a[j - 1]) ;
+			m = max(c, m);
 }

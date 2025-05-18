95b39a0edb4b0a7395256077aedc9ec44ba6c56d
Makariy Martyanov <makariy06@mail.ru>
2025-05-18 12:21:15 +0400

    feat: add binary search

--- binary search/f.cpp
+++ binary search/f.cpp
@@ -10,4 +10,8 @@ int main() {
 	for (int i = 0; i < n; ++i) cin >> a[i];
     sort(a.begin(), a.end());
 	double y, r = max(a[0], l - a[n - 1]);
+    for (int i = 1; i < n; ++i) {
+        y = (a[i ] - a[ i - 1]) / 2.0;
+        if (y > r) r = y;
+   }     
 }

be86d5b3f3cc95efe5d8a79753648a41bd6bb557
Makariy Martyanov <makariy06@mail.ru>
2025-05-26 19:47:49 +0400

    feat: add dfs

--- css/a.cpp
+++ css/a.cpp
@@ -6,3 +6,8 @@ int n;
 vector<vector<int>> g;
 vector<bool> used;
 vector<int>ord;
+void dfs(int v) {
+	used[v] = true;
+	for (int u = 0; u < n; ++u) if (!used[u] && g[v][u]) dfs(u);
+	ord.push_back(v);
+}

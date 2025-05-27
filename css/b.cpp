7bee9f84a6f376dbe7c358c5754d77d7e85bb49c
Makariy Martyanov <makariy06@mail.ru>
2025-05-27 19:02:32 +0400

    feat: add topsort

--- css/b.cpp
+++ css/b.cpp
@@ -5,3 +5,8 @@ using namespace std;
 vector<vector<int>> g, tg;
 vector<char> used;
 vector<int> ord;
+void ts(int v) {
+	used[v] = true;
+	for (int u : g[v]) if (!used[u]) ts(u);
+	ord.push_back(v);
+}
